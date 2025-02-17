60434bb72f58b2a18b60fe5521ec9533018b0469
Sandro Huber <sandro@sh-digital.ch>
2025-02-17 19:12:40 +0100

    django admin elemente

--- store/admin.py
+++ store/admin.py
@@ -2,11 +2,27 @@ from django.contrib import admin
 from .models import *
 from django.utils.translation import gettext_lazy as _
 
+
+class ElementeAdmin(admin.ModelAdmin):
+    list_display = ('elementnr', 'get_artikelnummer', 'get_kunde')  # Anzeige-Spalten
+    search_fields = ('elementnr', 'artikel__artikelnr', 'kunde__name')  # Suchfelder
+
+    def get_artikelnummer(self, obj):
+        """ Zeigt die Artikelnummer des verknüpften Artikels an """
+        return obj.artikel.artikelnr if obj.artikel else "Kein Artikel"
+    get_artikelnummer.short_description = "Artikelnummer"  # Spaltenüberschrift setzen
+
+    def get_kunde(self, obj):
+        """ Zeigt den Kunden des Elements an """
+        return obj.kunde.name if obj.kunde else "Kein Kunde"
+    get_kunde.short_description = "Kunde"
+
+    
 admin.site.register(Item)
 admin.site.register(OrderItem)
 admin.site.register(Order)
 admin.site.register(Address)
-admin.site.register(Elemente)
+admin.site.register(Elemente, ElementeAdmin)
 admin.site.register(Kunde)
 admin.site.register(Category)
 admin.site.register(Subcategory)
