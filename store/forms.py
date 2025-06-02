7b554e5fa6fc4d08685234a9e433b66797151f40
Sandro Huber <sandro@sh-digital.ch>
2025-06-02 16:57:41 +0200

    make change form

--- store/forms.py
+++ store/forms.py
@@ -1617,6 +1617,7 @@ class AussenmassForm(forms.ModelForm):
         }
 
 
+
 class MarkeChangeForm(forms.ModelForm):
     class Meta:
         model = Marke
@@ -1625,23 +1626,30 @@ class MarkeChangeForm(forms.ModelForm):
             'slug',
             'markepic',
             'marketext',
-            )
+            'bestseller',
+        )
         widgets = {
-            
             'name': forms.TextInput(attrs={
-                'class': 'form-control col-3',
-                'placeholder':''}),
-            'markepic' : forms.FileInput(attrs={
                 'class': 'form-control',
-                }),
-            
+                'placeholder': ''
+            }),
             'slug': forms.TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': ''}),
+                'placeholder': ''
+            }),
+            'markepic': forms.FileInput(attrs={
+                'class': 'form-control'
+            }),
             'marketext': forms.Textarea(attrs={
-                'class': 'form-control col-3',
-                'placeholder':''}),
+                'class': 'form-control',
+                'placeholder': ''
+            }),
+            'bestseller': forms.CheckboxInput(attrs={
+                'class': 'form-check-input',
+                'style': 'transform: scale(1.1); cursor: pointer;'
+            }),
         }
+
           
 
 
