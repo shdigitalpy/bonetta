9a95bed5cfd72169e737cc16007c033669af0d13
Md. Tasnif Rahman <77678799+tasnifrahman@users.noreply.github.com>
2025-06-04 09:03:08 +0600

    Update settings.py

--- webshop/settings.py
+++ webshop/settings.py
@@ -114,6 +114,7 @@ TEMPLATES = [
                 'store.context_processors.sub_extras', #for category display every
                 'store.context_processors.extras_marke', #for marke display every
                 'store.context_processors.first_cat',
+                'store.context_processors.marken_context',
             ],
         },
     },
